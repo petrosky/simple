8bf8008a2c6b9326b03e75526c895e7d4305d877
petrosky <1aIIbXXufd77ceKCyktU>
2018-12-14 17:06:58 +1100

    First commit

--- onemodel/urls.py
+++ onemodel/urls.py
@@ -7,6 +7,6 @@ app_name = 'onemodel'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
-    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/post_detail', views.post_detail, name='post_detail'),
     path('admin/', admin.site.urls),
 ]
